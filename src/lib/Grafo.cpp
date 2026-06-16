3438d41c939f8a1c88d4083c45b24b8410fc736a
GLKaiky <kaikyfranca8@gmail.com>
2026-06-16 14:08:33 -0300

    feat: Graph class and DFS implemented

--- src/lib/Grafo.cpp
+++ src/lib/Grafo.cpp
@@ -7,3 +7,51 @@
  * @copyright Copyright (c) 2026 Kaiky França dos Reis Silva
  */
 
+#include "Graph.hpp"
+#include <iostream>
+#include <stack>
+#include <vector>
+
+
+Graph::Graph(unsigned int number_of_vertex) {
+    this->number_of_vertex = number_of_vertex;
+    this->adjList.resize(number_of_vertex);
+}
+
+
+//Directec Edge
+void Graph::addEdge(unsigned int origin, unsigned int destination) {
+    this->adjList[origin].push_back(destination); //Point the origin vertex, to destination vertex A -> B -> C...
+}
+
+void Graph::removeEdge(unsigned int origin, unsigned int destination) {
+    this->adjList[origin].remove(destination); //Remove the edges between vertices
+}
+
+void Graph::DFS(unsigned int startVertex) {
+    std::stack<int> dfsStack;
+    std::vector<bool> visited(this->number_of_vertex, false);
+    
+    dfsStack.push(startVertex);
+    while(!dfsStack.empty()) {
+
+        int current_vertex = dfsStack.top();
+        
+        
+        dfsStack.pop();
+
+        if(!visited[current_vertex]){
+             visited [current_vertex] = true;
+
+            for(int neighbor : this->adjList[current_vertex]) {
+                if(!visited[neighbor]) {
+                    dfsStack.push(neighbor);    
+                }
+            }
+        }
+    }
+}  
+
+
+
+
--- src/lib/Graph.hpp
+++ src/lib/Graph.hpp
@@ -16,25 +16,25 @@
     class Graph {
         private:
             unsigned int number_of_vertex;
-            std::vector<std::list<int> > adjList; //Graph extructure
+            std::vector<std::list<int> > adjList; //Graph structure
+
+            void DFS(unsigned int startVertex); // Depth First Search
+ 
+            void BFS(); //Breadth First Search
 
         public:
             //Constructor
-            Graph(int number_of_vertex);
+            Graph(unsigned int number_of_vertex);
 
             //Add and Remove methods
-            void addEdge(unsigned int origin, unsigned int destiny);
-            void removeEdge(unsigned int origin, unsigned int destiny);
+            void addEdge(unsigned int origin, unsigned int destination);
+            void removeEdge(unsigned int origin, unsigned int destination);
 
             //Transitive reduction method. The goal is to remove the reduntant edges
             void execute_transitive_reduction();
 
             void printGraph();
-
-
-            void DFS(); // Deth First Search
- 
-            void BFS(); //Breadth First Search
+            
 
     };
 
